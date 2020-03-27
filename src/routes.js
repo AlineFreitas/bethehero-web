fa89c9559e90f3d008ec5150b5c9abf85633721a
Aline Freitas <aline.do.freitas@gmail.com>
2020-03-27 16:08:01 -0300

    Fixes spelling mistake on routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 export default function Routes() {
   return (
     <BrowserRouter>
         <Switch>
           <Route exact path="/" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
         </Switch>
     </BrowserRouter>
   );
