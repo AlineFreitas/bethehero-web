d48147da1eda8d69c23da4a397aa63571ce08f27
Aline Freitas <aline.do.freitas@gmail.com>
2020-03-27 14:32:24 -0300

    Added import for images on Singin and updated the sources

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import heroesLogo from '../../assets/logo.svg';
+import heroesBanner from '../../assets/heroes.png';
+
 import './sign_in.css';
 
 export default function SignIn() {
   return(
     <div className="signup-container">
       <section className="form">
-        <img alt="Be The Hero Logo" />
+        <img src={heroesLogo} alt="Be The Hero Logo" />
 
         <form>
           <h2>Log on</h2>
@@ -16,7 +19,7 @@ export default function SignIn() {
         </form>
       </section>
 
-      <img alt="Be The Hero Logo" />
+      <img src={heroesBanner} alt="Be The Hero" />
     </div>
   );
 }
