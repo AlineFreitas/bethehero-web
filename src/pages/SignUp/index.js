dd3523ba1d76cc152cd0949a2f5944ed7e791888
Aline Freitas <aline.do.freitas@gmail.com>
2020-03-27 16:52:25 -0300

    Created SignUp page with structure and textual content

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export default function SignUp() {
+  console.log("welcome");
+
+  return(
+    <div className="signup-container">
+      <div className="content">
+        <section>
+          <img alt="Be The Hero Logo" />
+
+          <h2>Sign Up</h2>
+
+          <p>
+            Sign up, then sign in to the plattaform and help people find your NGO's cases.
+          </p>
+          <a href="/">
+            Return to Sign In
+          </a>
+        </section>
+
+        <form>
+          <input placeholder="NGO's name" />
+          <input type="email" placeholder="E-mail" />
+          <input placeholder="Whatsapp" />
+          <input placeholder="City" />
+          <input placeholder="UF" />
+
+          <button>
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
