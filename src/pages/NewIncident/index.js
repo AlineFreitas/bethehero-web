d75bbc945588fbbe332a9a5531a0fe64c0edbe06
Aline Freitas <aline.do.freitas@gmail.com>
2020-03-27 20:46:26 -0300

    Adds structure and text to New Incident's page

--- src/pages/NewIncident/index.js
+++ src/pages/NewIncident/index.js
@@ -2,6 +2,30 @@ import React from 'react';
 
 export default function NewIncident(){
   return (
-    <h1>New Incident</h1>
+    <div className="new-incident-container">
+      <div className="content">
+        <section>
+          <img alt="Be The Hero Logo" />
+
+          <h2>Register New Incident</h2>
+          <p>
+            Relate the incident in detail to help you find a hero to solve it.
+          </p>
+          <a className="back-icon-link" href="/">
+            Return Home
+          </a>
+        </section>
+
+        <form>
+          <input placeholder="Incident title" />
+          <textarea placeholder="Description" />
+          <input placeholder="Amount (in USD)" />
+
+          <button className="button">
+            Register
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
