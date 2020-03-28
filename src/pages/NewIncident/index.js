20f293e030da4ef47c0025938600f3d34656c4d8
Aline Freitas <aline.do.freitas@gmail.com>
2020-03-27 21:02:04 -0300

    Updated New Incident's page with icons, images, links and style

--- src/pages/NewIncident/index.js
+++ src/pages/NewIncident/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import { FiArrowLeft } from 'react-icons/fi';
+
+import heroesLogo from '../../assets/logo.svg';
+
+import './new_incident.css'
+
 
 export default function NewIncident(){
   return (
     <div className="new-incident-container">
       <div className="content">
         <section>
-          <img alt="Be The Hero Logo" />
+          <img src={heroesLogo} alt="Be The Hero Logo" />
 
           <h2>Register New Incident</h2>
           <p>
             Relate the incident in detail to help you find a hero to solve it.
           </p>
-          <a className="back-icon-link" href="/">
-            Return Home
-          </a>
+          <Link className="back-icon-link" to="/">
+            <FiArrowLeft size={16} color="#e02041" /> Return Home
+          </Link>
         </section>
 
         <form>
