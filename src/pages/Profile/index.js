1b0ed7fcf46de622febf114770f04fa22c19070a
Aline Freitas <aline.do.freitas@gmail.com>
2020-03-28 01:47:39 -0300

    Connects profile page to backend, and dinamically renders page's content

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
+
+import api from '../../services/api'
 
 import heroesLogo from '../../assets/logo.svg';
 
 import './profile.css';
 
 export default function Profile(){
+  const ongName = localStorage.getItem('ngoName');
+  const [incidents, setIncidents] = useState([]);
+
+  useEffect(() =>{
+    const ngoID = localStorage.getItem('ngoID');
+    api.get(`/ngos/${ngoID}/incidents`, {
+      headers: {
+        Authorization: ngoID
+      }
+    }).then( response => {
+      setIncidents(response.data); 
+    });
+  }, []);
+
   return(
     <div className="profile-container">
       <header>
         <img src={heroesLogo} alt="Be The Hero Logo" />
-        <span>Welcome, Lambeijinhos!</span>
+        <span>Welcome, {ongName}!</span>
 
         <a className="button" href="/incidents/new">Add new Incident</a>
 
@@ -21,84 +37,27 @@ export default function Profile(){
 
       <h2>Registered Incidents</h2>
       <ul>
-        <li>
-          <strong>Title label</strong>
-          <p>title</p>
+        { incidents.map( incident => {
+          return (
+            <li key={incident.id}>
+              <strong>Incident</strong>
+              <p>{incident.title}</p>
 
-          <strong>Description label</strong>
-          <p>Long text with the whole case description to touch our heroes hearts</p>
+              <strong>Description</strong>
+              <p>{incident.description}</p>
 
-          <strong>Value label</strong>
-          <p>$###,##</p>
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3"/>
-          </button>
-        </li>
-        <li>
-          <strong>Title label</strong>
-          <p>title</p>
+              <strong>Value</strong>
+              <p>
+                {Intl.NumberFormat('en-us',
+                { style: 'currency', currency: 'BRL'}).format(incident.value)}
+              </p>
 
-          <strong>Description label</strong>
-          <p>Long text with the whole case description to touch our heroes hearts</p>
-
-          <strong>Value label</strong>
-          <p>$###,##</p>
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3"/>
-          </button>
-        </li>
-        <li>
-          <strong>Title label</strong>
-          <p>title</p>
-
-          <strong>Description label</strong>
-          <p>Long text with the whole case description to touch our heroes hearts</p>
-
-          <strong>Value label</strong>
-          <p>$###,##</p>
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3"/>
-          </button>
-        </li>
-        <li>
-          <strong>Title label</strong>
-          <p>title</p>
-
-          <strong>Description label</strong>
-          <p>Long text with the whole case description to touch our heroes hearts</p>
-
-          <strong>Value label</strong>
-          <p>$###,##</p>
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3"/>
-          </button>
-        </li>
-        <li>
-          <strong>Title label</strong>
-          <p>title</p>
-
-          <strong>Description label</strong>
-          <p>Long text with the whole case description to touch our heroes hearts</p>
-
-          <strong>Value label</strong>
-          <p>$###,##</p>
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3"/>
-          </button>
-        </li>
-        <li>
-          <strong>Title label</strong>
-          <p>title</p>
-
-          <strong>Description label</strong>
-          <p>Long text with the whole case description to touch our heroes hearts</p>
-
-          <strong>Value label</strong>
-          <p>$###,##</p>
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3"/>
-          </button>
-        </li>
+              <button type="button">
+                <FiTrash2 size={20} color="#a8a8b3"/>
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
