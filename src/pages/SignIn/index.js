85773e23c40b2b695b9e5418469c0181a8a132d9
Aline Freitas <aline.do.freitas@gmail.com>
2020-03-27 22:26:53 -0300

    Updated sign in page to connect to backend, storing NGO's id and name on localstorage

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,8 @@
-import React from 'react'; 
+import React, { useState } from 'react'; 
 import { Link } from 'react-router-dom';
-
 import { FiLogIn } from 'react-icons/fi';
+
+import api from '../../services/api'
 
 import heroesLogo from '../../assets/logo.svg';
 import heroesBanner from '../../assets/heroes.png';
@@ -9,14 +10,37 @@ import heroesBanner from '../../assets/heroes.png';
 import './sign_in.css';
 
 export default function SignIn() {
+
+  const [id, setID] = useState('');
+
+  async function handleSignIn(event){
+
+    event.preventDefault();
+
+    try {
+      const response = await api.post('/sessions', { id });
+
+      localStorage.setItem('ngoID', id);
+      localStorage.setItem('ngoName', response.data.name);
+
+    } catch(error) {
+      alert('Login failed. Try again');
+    }
+
+  }
+  
   return(
     <div className="signin-container">
       <section className="form">
         <img src={heroesLogo} alt="Be The Hero Logo" />
 
-        <form>
+        <form onSubmit={handleSignIn}>
           <h2>Log on</h2>
-          <input placeholder="Your ID"/>
+          <input
+            value={id}
+            onChange={ event => setID(event.target.value)}
+            placeholder="Your ID"
+          />
           <button className="button" type="submit">Sign In</button>
           
           <Link className="back-icon-link" to="/signup">
